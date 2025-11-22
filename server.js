39d1f9c2db3c69298f6becfe379f5593d7abe18a
Abd Eltawab Shaaban <105607290+scob337@users.noreply.github.com>
2025-11-22 08:32:29 +0000

    000

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.get('/', (req, res) => {
+  console.log("Hello World");
+  res.send('Server running — check console.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT : ${PORT}`);
+});
